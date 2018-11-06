275b108d7739d60ef320e6419a29e45a2ab4fb94
Kalikalyan Dash <kkdgrece@gmail.com>
2018-11-05 23:00:02 -0800

    hw2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import requests
 app = Flask(__name__)
 
 
@@ -9,24 +10,37 @@ def homepage():
 @app.route('/answer',methods = ['POST', 'GET'])
 def answer():
    if request.method == 'POST':
-   	buy_commission = int((request.form['buy_commission']))
-   	initial_share_price = int((request.form['initial_share_price']))
-   	allotment = int((request.form['allotment']))
-   	final_share_price = int((request.form['final_share_price']))
-   	sell_commission = int((request.form['sell_commission']))
-   	tax_rate = int((request.form['capital_gain_tax_rate']))
-   	proceeds = allotment*final_share_price
-   	total_purchase_price = allotment*initial_share_price
-   	initial_investment = (total_purchase_price + buy_commission + sell_commission)
-   	taxable_gain = proceeds - initial_investment
-   	tax =(float) (tax_rate*taxable_gain)/100
-   	cost = initial_investment + tax
-   	net_profit = taxable_gain - tax
-   	roi = '{0:.2f}%'.format((net_profit)*100/(initial_investment+tax))
-   	break_even_share_price = (float)(initial_share_price + (float)(sell_commission + buy_commission)/allotment)
-
-   	return render_template("answer.html",p = proceeds, c = cost, tpp = total_purchase_price, bc = buy_commission, sc = sell_commission, t = tax, np = net_profit, ri = roi, brk = break_even_share_price)
+	symbol = request.form['symbol']
+	print(symbol)
+	symbol = symbol.upper()
+	print(symbol)
+	r = requests.get(
+		   'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + symbol + '&apikey=O0VYSACAKHVTFZ1P')
+	data = r.json()
+	metaData = data['Meta Data']
+	lastRefreshed = metaData['3. Last Refreshed']
+	timeZone = metaData['5. Time Zone']
+	dateTime = lastRefreshed + " " + timeZone
+	stockInfo = data['Time Series (Daily)']
+	lastDataSet = stockInfo['2018-11-05']
+	openingStockPrice = lastDataSet['1. open']
+	currentStockPrice = lastDataSet['4. close']
+	stockDelta = float(currentStockPrice) - float(openingStockPrice)
+	url = "http://d.yimg.com/autoc.finance.yahoo.com/autoc?query={}&region=1&lang=en".format(symbol)
+	result = requests.get(url).json()
+	for x in result['ResultSet']['Result']:
+		if x['symbol'] == symbol:
+			company = x['name']
+	companyName = company
+	print('STOCK REPORT')
+	print(dateTime + "\n")
+	print("{} ({})\n".format(companyName, symbol))
+	percentChange = stockDelta / float(openingStockPrice) * float(100)
+	if stockDelta < 0:
+		printThis = "{} {} ({}%)\n".format(currentStockPrice, round(stockDelta, 2), round(percentChange, 2))
+	else:
+		printThis = "{} +{} (+{}%)\n".format(currentStockPrice, round(stockDelta, 2), round(percentChange, 2))
+   	return render_template("answer.html",companyName = companyName, symbol = symbol, printThis = printThis)
 
 if __name__ == '__main__':
    app.run(debug = True)
-
